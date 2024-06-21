0127b60e0f2bf2eba2b2daeb53cb8bf65191c0ff
vinonsan9744 <asp2020131@as.rjt.ac.lk>
2024-06-21 14:11:38 +0530

    login and register

--- src/pages/react/LoginPage.jsx
+++ src/pages/react/LoginPage.jsx
@@ -1,9 +1,14 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from 'react';
 import "./../style/LoginPage.css";
-
+import Button from 'react-bootstrap/Button';
 
 function LoginPage() {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleOptionChange = (event) => {
+      setSelectedOption(event.target.value);
+  };
   return (
     <>
       <div className="container-flex vh-100">
@@ -13,11 +18,11 @@ function LoginPage() {
           <div className="LoginPage-main-left col-sm-12 col-md-6 col-lg-6 col-xl-6 ">
             
              <div className="hazard-LoginPage-header-box container-flex">
-             {/* <div className="hazard-LoginPage-header-title">Sri Lanka Railway Safety System</div> */}
+             <div className="hazard-LoginPage-header-title">SriLankan Railway Safety System</div>
              </div>
 
              <div className="hazard-LoginPage-description-box container-flex">
-             {/* <div className="hazard-LoginPage-header-title">Sri Lanka Railway Safety System</div> */}
+             <div className="hazard-LoginPage-header-description"><p>The login process for our Railway Safety System is straightforward and secure. Locomotive pilots or admins simply input their email and password on the login page. The system then verifies these details, ensuring only authorized personnel gain access. Upon successful verification, users are redirected to the main home page, where they can access tailored options and settings designed to facilitate a seamless navigation experience within the system.</p></div>
              </div>
 
              <div className="hazard-LoginPage-line-box container-flex">
@@ -45,7 +50,7 @@ function LoginPage() {
           <div className="LoginPage-main-right col-sm-12 col-md-6 col-lg-6 col-xl-6 ">
 
              <div className="hazard-LoginPage-heading-box container-flex">
-             {/* <div className="hazard-LoginPage-header-title">Sri Lanka Railway Safety System</div> */}
+             <div className="hazard-LoginPage-heading-title">Login Form</div>
              </div>
 
              <div className="hazard-LoginPage-heading-line-box container-flex">
@@ -53,27 +58,56 @@ function LoginPage() {
              </div>
 
              <div className="hazard-LoginPage-radio-box container-flex">
-             {/* <div className="hazard-LoginPage-header-title">Sri Lanka Railway Safety System</div> */}
+             <div className="hazard-LoginPage-radio-container">
+            <label className="hazard-LoginPage-radio-label">
+                <input
+                    type="radio"
+                    value="option1"
+                    checked={selectedOption === 'option1'}
+                    onChange={handleOptionChange}
+                />
+                <span className="hazard-LoginPage-custom-radio"></span>
+                Locomotive pilot
+            </label>
+            <label className="hazard-LoginPage-radio-label">
+                <input
+                    type="radio"
+                    value="option2"
+                    checked={selectedOption === 'option2'}
+                    onChange={handleOptionChange}
+                />
+                <span className="hazard-LoginPage-custom-radio"></span>
+                Admin
+            </label>
+        </div>
              </div>
 
              <div className="hazard-LoginPage-text-box container-flex">
-             {/* <div className="hazard-LoginPage-header-title">Sri Lanka Railway Safety System</div> */}
+             <input
+                type="text"
+                placeholder="Enter  User Name"
+                className="hazard-LoginPage-input-text-box"
+              />
              </div>
 
              <div className="hazard-LoginPage-text-box container-flex">
-             {/* <div className="hazard-LoginPage-header-title">Sri Lanka Railway Safety System</div> */}
+             <input
+                type="password"
+                placeholder="Enter  Password"
+                className="hazard-LoginPage-input-text-box"
+              />
              </div>
 
              <div className="hazard-LoginPage-login-button-box container-flex">
-             {/* <div className="hazard-LoginPage-header-title">Sri Lanka Railway Safety System</div> */}
+             <Button className="hazard-LoginPage-login-button">Sign in</Button>
              </div>
 
              <div className="hazard-LoginPage-register-button-box container-flex">
-             {/* <div className="hazard-LoginPage-header-title">Sri Lanka Railway Safety System</div> */}
+             <Button className="hazard-LoginPage-signup-button">Sign up</Button>
              </div>
 
              <div className="hazard-LoginPage-forgot-label-box container-flex">
-             {/* <div className="hazard-LoginPage-header-title">Sri Lanka Railway Safety System</div> */}
+             <div className="hazard-LoginPage-forgot-label">Forgot password?</div>
              </div>
 
           </div>
